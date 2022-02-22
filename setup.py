6115d849dc7f5f2c2a0a38538a9a4653181c76c4
Manuel Olguín Muñoz <manuel@olguin.se>
2022-02-22 13:12:26 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 import setuptools
 
-reqs = []
 with open('./requirements.txt', 'r') as f:
-    for line in f:
-        reqs.append(line.strip())
+    reqs = [line.strip() for line in f]
 
 setuptools.setup(
     name="gabriel_lego",
-    version="0.1.12",
+    version="0.2.0",
     author="Manuel Olguin Munoz",
     author_email="molguin@kth.se",
     description="Gabriel LEGO Assembly cognitive engine.",
     long_description_content_type="text/markdown",
-    url="https://github.com/cmusatyalab/gabriel-lego-py3",
+    url="https://github.com/molguin92/gabriel-lego-py3",
     packages=setuptools.find_packages(exclude=['gabriel-server*']),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,5 +19,5 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
     ],
     python_requires='>=3.7',
-    install_requires=reqs
+    install_requires=reqs,
 )
